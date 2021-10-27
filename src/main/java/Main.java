f3906a0a836076a3846af8913fddefb0e7a6d29a
Kateryna Salamatina <kateryna.salamatina@gmail.com>
2021-10-27 16:06:48 +0300

    Adding first version of the project

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Main {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "/Users/katerinasalamatina/Desktop/task8/src/main/resources/chromedriver");
+
+        WebDriver driver = new ChromeDriver();
+
+    }
+}
